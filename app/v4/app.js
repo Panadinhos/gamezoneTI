cf50d694bd9c5d8ef507c01d1e9ea385d8ef5843
Panadinhos <zacarini@gmail.com>
2021-01-01 03:27:10 +0000

    v4 - Tentativa falahada da implementação do xml. Simplificação da escrita do app.js.

---
+++ app/v4/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs')
+
+app.get('/home', function (req, res) {
+    res.render('home');
+    })
+
+app.get('/jogos', function (req, res) {
+    res.render('jogos');
+})
+
+app.get('/perifericos', function (req, res) {
+    res.render('perifericos');
+})
+
+app.get('/descontos', function (req, res) {
+    res.render('descontos');
+})
+
+app.get('*', function (req, res) {
+    res.render('home')
+})
+
+app.listen(8080, () => {
+    console.log('A OUVIR NO PORTO 8080')
+})
