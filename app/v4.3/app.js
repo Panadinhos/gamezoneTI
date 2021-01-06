40b21d797f9e766f6b2163b1a4ca8fe715d83b67
Panadinhos <zacarini@gmail.com>
2021-01-06 20:31:05 +0000

    v4.3 - Nova tentativa de implementacao do xml utilizando a bliblioteca fs disponive na npm.

---
+++ app/v4.3/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+var gzd = new Object();
+
+fs.readFile('./data/gamezone.xml', 'utf8', function(err, data){
+
+  if(err) {
+    console.error("ERROR");
+  }
+
+  console.log("OK");
+  gzd = data;
+})
+
+console.log(gzd);
+
+
+app.set('view engine', 'ejs')
+
+app.get('/home', function (req, res) {
+    res.render('home');
+    })
+
+app.get('/jogos', function (req, res) {
+    res.render('jogos');
+})
+
+app.get('/perifericos', function (req, res) {
+    res.render('perifericos');
+})
+
+app.get('/descontos', function (req, res) {
+    res.render('descontos');
+})
+
+app.get('*', function (req, res) {
+    res.render('home')
+})
+
+app.listen(8080, () => {
+    console.log('A OUVIR NO PORTO 8080')
+})
