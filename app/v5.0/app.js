db6663a7b3e150758f151e8353f87c80aa3f8979
Panadinhos <zacarini@gmail.com>
2021-01-07 20:21:03 +0000

    v5.0 - Parse do xml feito com sucesso

---
+++ app/v5.0/app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+var gzdxml = new Object();
+
+fs.readFile('data/gamezone.xml', 'utf8', function(err, data){
+
+  if(err) {
+    console.error("ERROR");
+  }
+
+  console.log("OK");
+  gzdxml = data;
+  console.log(gzdxml);
+});
+
+const xml2js = require('xml2js');
+
+var parser = new xml2js.Parser();
+parser.parseStringPromise(gzdxml).then(function (result) {
+  console.dir(result);
+  console.log('Done');
+})
+.catch(function (err) {
+  // Failed
+});
+
+
+app.set('view engine', 'ejs')
+
+app.get('/home', function (req, res) {
+    res.render('home',{jogos:gzd});
+})
+
+app.get('/jogos', function (req, res) {
+    res.render('jogos');
+})
+
+app.get('/perifericos', function (req, res) {
+    res.render('perifericos');
+})
+
+app.get('/descontos', function (req, res) {
+    res.render('descontos');
+})
+
+app.get('*', function (req, res) {
+    res.render('home',{jogos:gzd});
+})
+
+app.listen(8080, () => {
+    console.log('A OUVIR NO PORTO 8080')
+})
