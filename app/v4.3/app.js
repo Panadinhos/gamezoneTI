ea20a866f6183548f37f234012f031a4c9f55bdc
Panadinhos <zacarini@gmail.com>
2021-01-07 16:41:44 +0000

    Faltava um ;

--- app/v4.3/app.js
+++ app/v4.3/app.js
@@ -17,28 +17,28 @@ fs.readFile('data/gamezone.xml', 'utf8', function(err, data){
 console.log(gzd);
 
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.get('/home', function (req, res) {
     res.render('home',{jogos:gzd});
-    })
+});
 
 app.get('/jogos', function (req, res) {
     res.render('jogos');
-})
+});
 
 app.get('/perifericos', function (req, res) {
     res.render('perifericos');
-})
+});
 
 app.get('/descontos', function (req, res) {
     res.render('descontos');
-})
+});
 
 app.get('*', function (req, res) {
     res.render('home')
-})
+});
 
 app.listen(8080, () => {
     console.log('A OUVIR NO PORTO 8080')
-})
+});
