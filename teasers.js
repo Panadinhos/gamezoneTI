d4b74aac4a25bd3b38ddb5e25b4e3092f95078e5
Ana Machado <anarnmachado@gmail.com>
2021-01-10 00:34:22 +0000

    teasers

---
+++ teasers.js
@@ -0,0 +1,11 @@
+function Teaser()
+{
+    $(document).ready(function()
+    {
+        $("#btn").click(function(){
+            alert('Cuidado!');
+            console.log('Está prestes a abandonar Gamezone!');
+        });
+    });
+}
+Teaser();
