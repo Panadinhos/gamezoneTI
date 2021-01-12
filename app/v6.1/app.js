df4f82bfec919e99b5f996070b6d531b16e385b1
Ana Machado <anarnmachado@gmail.com>
2021-01-12 01:42:20 +0000

    correcoes server

--- app/v6.1/app.js
+++ app/v6.1/app.js
@@ -1,63 +1,67 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+const xml2js = require('xml2js');
+const parser = new xml2js.Parser({ attrkey: "ATTR" });
+//var gzdxml = new Object();
 
-var gzdxml = new Object();
+var stringgg = fs.readFileSync('a.xml', 'utf-8')
+parser.parseString(stringgg, (err, d) => {
+  if (err === null) {
+    console.log(d["gamezone"]["game"][0]);
 
-fs.readFile('data/gamezone.xml', 'utf8', function(err, data){
-
-  if(err) {
-    console.error("ERROR");
+  } else {
+    console.log(err)
   }
 
   console.log("OK");
-  gzdxml = data;
-  console.log(gzdxml);
+  //gzdxml = data;
+  //console.log(gzdxml);
 });
 
-const xml2js = require('xml2js');
-
-var parser = new xml2js.Parser();
-parser.parseStringPromise(gzdxml).then(function (result) {
-  console.dir(result);
-  console.log('Done');
-})
-.catch(function (err) {
-  // Failed
-});
+//const xml2js = require('xml2js');
 
 
-app.set('view engine', 'ejs')
+// parser.parseStringPromise(gzdxml).then(function (result) {
+//   console.dir(result);
+//   console.log('Done');
+// })
+// .catch(function (err) {
+//   // Failed
+// });
 
 
-app.get('/jogos', function (req, res) {
-    res.render('jogos');
-})
+// app.set('view engine', 'ejs')
 
-app.get('/perifericos', function (req, res) {
-    res.render('perifericos');
-})
 
-app.get('/descontos', function (req, res) {
-    res.render('descontos');
-})
+// app.get('/jogos', function (req, res) {
+//     res.render('jogos');
+// })
 
-app.get('/plat', function (req, res) {
-  res.render('plat');
-})
+// app.get('/perifericos', function (req, res) {
+//     res.render('perifericos');
+// })
 
-app.get('/criadores', function (req, res) {
-  res.render('criadores');
-})
+// app.get('/descontos', function (req, res) {
+//     res.render('descontos');
+// })
 
-app.get('/eventos', function (req, res) {
-  res.render('eventos');
-})
+// app.get('/plat', function (req, res) {
+//   res.render('plat');
+// })
 
-app.get('*', function (req, res) {
-    res.render('jogos',{jogos:gzdxml});
-})
+// app.get('/criadores', function (req, res) {
+//   res.render('criadores');
+// })
 
-app.listen(8080, () => {
-    console.log('A OUVIR NO PORTO 8080')
-})
+// app.get('/eventos', function (req, res) {
+//   res.render('eventos');
+// })
+
+// app.get('*', function (req, res) {
+//     res.render('jogos');
+// })
+
+// app.listen(8080, () => {
+//     console.log('A OUVIR NO PORTO 8080')
+// })
